13b9deaebd6e5b995e96e1cd7c7ff7dc74e453eb
Murilo Nascimento <murilojesus659@gmail.com>
2022-11-25 12:17:12 -0300

    Blog Pessoal Front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from "./components/estaticos/navbar/Navbar";
+import { About } from './paginas/about/about';
+import { Home } from "./paginas/home/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>  // Orquestrador - que monta a página que o usuário irá utilizar
+      <Navbar />
+      <Routes>  // Lista de rotas da aplicação
+        <Route path='/' element={<Home />} /> // Rota
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
